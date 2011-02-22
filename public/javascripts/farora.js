401460f9a6b6ce766c5accdd4e3652f03509a5b1
Eumir Gaspar <eumir@aelogica.com>
2011-02-23 05:47:25 +0800

    farora: semi dirty fix for container div added jquery new fields for services

--- public/javascripts/farora.js
+++ public/javascripts/farora.js
@@ -1,3 +1,15 @@
+$(function(){
+  var serviceNumber = 1;
+
+  cloneField = function(selector, newId) {
+    var newField = $(selector).clone();
+    newField.attr("id", newId+"-"+serviceNumber);
+    newField = $('<div>').append(newField.clone()).remove().html();
+    serviceNumber++;
+    return newField;
+  }
+})
+
 $(document).ready(function() {
   $('#date').datepicker({
     inline: true,
@@ -5,7 +17,13 @@ $(document).ready(function() {
     minDate: 0
   });
   
-  $("#start-time").calendricalTime();
-  $("#end-time").calendricalTime();
   
+  $(".add-service").click( function(){
+    serviceSelect = cloneField(".type-of-service select", "service-type");
+    stylistSelect = cloneField(".stylist select", "stylist-name");
+    priceInput = cloneField(".price:first input", "price-for");
+    var cloneHtml = "<li class='additional-services'><label>Type of Service</label>" + serviceSelect + "</li><li><label>Stylist</label>" + stylistSelect + "</li><li class='no-count price'><label>Price</label>" + priceInput + "</li>";
+    $(".after-count").before(cloneHtml);
+    return false;
+  });
 });
